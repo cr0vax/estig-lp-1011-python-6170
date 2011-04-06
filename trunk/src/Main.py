e76b5d53bc15cc224053925452e00ff8ac1a5fcd
bruno.as.moreira@gmail.com <bruno.as.moreira@gmail.com@c586d81c-5235-2403-ea5e-bef011c130c5>
2011-04-06 20:55:03 +0000

    Base de dados com IDs

--- trunk/src/Main.py
+++ trunk/src/Main.py
@@ -7,11 +7,18 @@ class Main:
     def __init__(self):
         self.bd = BaseDados()
         pass
-    pass    
+    pass
     
+    '''
+    Collects data from a specified year
+    
+        ano - year of the data to be collected
+    '''
     def collect_data(self, ano):
+        print "STARTED COLLECTING DATA FOR YEAR", ano
         self.bd.criar_tabelas()
         self.bd.preencher_tipos_estabelecimento(ano)
         self.bd.preencher_tabelas_gerais(ano)
+        print "FINISHED COLLECTING DATA FOR YEAR", ano
         pass
     pass
