4761a7139d5793f5a280ef4222b8e5f945e245f3
bruno.as.moreira <bruno.as.moreira@gmail.com>
2011-04-10 19:12:30 +0000

    Count Statistics completed. HTTP server added

--- src/BaseDados.py
+++ src/BaseDados.py
@@ -3,99 +3,75 @@
 import sqlite3
 from ObterDados import ObterDados
 
-########################################
-# operacoes com base de dados
-########################################
+#---------------------------------------
+# Operacoes de criação da base de dados
+# Autor: Bruno Moreira
+# Número: 6170
+#---------------------------------------
 
 class BaseDados:
     
+    #---------------------
+    # Construtor da classe
+    #---------------------
     def __init__(self):
         self.dados = ObterDados()
         pass
     pass
 
+
+    #---------------------------------
+    # Cria as tabelas da base de dados
+    #---------------------------------
     def criar_tabelas(self):
-        '''
-        criacao das tabelas da base de dados
-        '''
+
+        # inicia a conexão à base de dados
         conn = sqlite3.connect('rebides.db')
         cursor = conn.cursor()
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS graus 
-##                          (designacao TEXT PRIMARY KEY)
-##                       """)
-
+        
+        # tabela graus
         cursor.execute("""CREATE TABLE IF NOT EXISTS graus
                 ("id_grau" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                  "designacao" TEXT NOT NULL);
                 """)
 
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS cursos
-##                        ("id_curso" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-##                        "designacao" TEXT PRIMARY KEY)
-##                       """)
-
+        # tabela cursos
         cursor.execute("""CREATE TABLE IF NOT EXISTS cursos
                         ("id_curso" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                         "designacao" TEXT NOT NULL)
                        """)
                     
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS categorias
-##                        ("id_categoria" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-##                        "designacao" TEXT PRIMARY KEY)
-##                       """)
-
+        # tabela categorias
         cursor.execute("""CREATE TABLE IF NOT EXISTS categorias
                         ("id_categoria" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                         "designacao" TEXT NOT NULL)
                        """)
-                    
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS regimes
-##                          ("id_regime" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-##                          "designacao" TEXT PRIMARY KEY)
-##                       ""select * from fichas_docencia")
-
+        
+        # tabela regimes
         cursor.execute("""CREATE TABLE IF NOT EXISTS regimes
                           ("id_regime" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                           "designacao" TEXT NOT NULL)
                        """)
                     
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS estabelecimentos
-##                          ("id_estabelecimento" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-##                          "designacao" TEXT PRIMARY KEY)
-##                       """)
-                    
+        # tabela estabelecimentos         
         cursor.execute("""CREATE TABLE IF NOT EXISTS estabelecimentos
                           ("id_estabelecimento" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                           "designacao" TEXT NOT NULL)
                        """)
                     
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS tipos_estabelecimento
-##                          ("id_tipo_estabelecimento" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-##                          "designacao" TEXT PRIMARY KEY)
-##                       """)
-
+        # tabela tipos_estabelecimentos
         cursor.execute("""CREATE TABLE IF NOT EXISTS tipos_estabelecimento
                           ("id_tipo_estabelecimento" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                           "designacao" TEXT NOT NULL)
                        """)
                     
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS docentes
-##                          (nome_completo TEXT PRIMARY KEY, 
-##                           codigo INTEGER UNIQUE)
-##                       """)
+        # tabela docentes
         cursor.execute("""CREATE TABLE IF NOT EXISTS docentes
                           ("nome_completo" TEXT, 
                            "id_docente" INTEGER PRIMARY KEY)
                        """)
                     
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS fichas_curso
-##                          (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-##                           ano INTEGER,
-##                           docente REFERENCES docentes (nome_completo),
-##                           grau REFERENCES graus (designacao),
-##                           curso REFERENCES cursos (designacao))
-##                       """)
-                    
+        # tabela fichas_curso
         cursor.execute("""CREATE TABLE IF NOT EXISTS fichas_curso
                           (id INTEGER PRIMARY KEY AUTOINCREMENT, 
                            ano INTEGER,
@@ -104,16 +80,7 @@ class BaseDados:
                            id_curso INTEGER REFERENCES cursos (id_curso))
                        """)
         
-##        cursor.execute("""CREATE TABLE IF NOT EXISTS fichas_docencia
-##                          (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-##                           ano INTEGER,
-##                           docente REFERENCES docentes (nome_completo),
-##                           estabelecimento REFERENCES estabelecimentos (designacao),
-##                           categoria REFERENCES categorias (designacao),
-##                           regime REFERENCES regimes (designacao),
-##                           tipo_estabelecimento REFERENCES 
-##                           tipos_estabelecimento (designacao))
-##                       """)
+        # tabela fichas_docencia
         cursor.execute("""CREATE TABLE IF NOT EXISTS fichas_docencia
                           (id INTEGER PRIMARY KEY AUTOINCREMENT, 
                            ano INTEGER,
@@ -129,8 +96,14 @@ class BaseDados:
 
         cursor.close()
         pass
+    pass
 
 
+    #----------------------------------------
+    # Preenche os tipos de estabelecimento
+    #
+    #   ano - número do ano a ser preenchido
+    #----------------------------------------
     def preencher_tipos_estabelecimento(self, ano):
         '''
         preenchimento da tabela tipos_estabelecimento
@@ -144,7 +117,8 @@ class BaseDados:
 
         for tipo, codR in d_tipos_estabelecimento.iteritems():
             try:
-                cmd = """insert into tipos_estabelecimento (designacao) values ('{0}')""".format(tipo.replace("'", "''", 1))
+                cmd = """insert into tipos_estabelecimento (designacao) 
+                        values ('{0}')""".format(tipo.replace("'", "''"))
                 cursor.execute(cmd)
                 conn.commit()
             except:
@@ -153,13 +127,20 @@ class BaseDados:
         cursor.close()
         pass
 
+    #----------------------------------------
+    # Preenche os estabelecimentos
+    #
+    #   conn               - conexão à bd
+    #   cursor             - cursor com dados
+    #   d_estabelecimentos - lista com estabelecimentos
+    #----------------------------------------
     def preencher_estabelecimentos(self, conn, cursor, d_estabelecimentos):
         '''
         preenchimento da tabela estabelecimentos
         '''
         for estabelecimento, codP in d_estabelecimentos.iteritems():
             cmd = """insert into {0} (designacao) values ('{1}')""".\
-                format('estabelecimentos', estabelecimento.replace("'", "''", 1))
+                format('estabelecimentos', estabelecimento.replace("'", "''"))
             
             try:
                 cursor.execute(cmd)
@@ -179,7 +160,7 @@ class BaseDados:
 
         for x in lista:
             cmd = """insert into {0} (designacao) values ('{1}')""".\
-                format(tipo, x.replace("'", "''", 1))
+                format(tipo, x.replace("'", "''"))
 
             try:
                 cursor.execute(cmd)
@@ -200,7 +181,7 @@ class BaseDados:
             for x in linha:
                 #cmd = """insert into {0} (codigo, nome_completo) values ({1}, '{2}')""".\
                 cmd = """insert into {0} (id_docente, nome_completo) values ({1}, '{2}')""".\
-                    format('docentes', x[C_CODIGO], x[C_DOCENTE].replace("'", "''", 1))
+                    format('docentes', x[C_CODIGO], x[C_DOCENTE].replace("'", "''"))
                 #print cmd
                 try:
                     cursor.execute(cmd)
@@ -250,7 +231,7 @@ class BaseDados:
                 # selecciona a referência do grau
                 #cmd = """SELECT designacao FROM graus WHERE designacao = '{0}'""".\
                 cmd = """SELECT id_grau FROM graus WHERE designacao = '{0}'""".\
-                    format(x[C_GRAU].replace("'", "''", 1))
+                    format(x[C_GRAU].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -261,7 +242,7 @@ class BaseDados:
                 # selecciona a referência do curso
                 #cmd = """SELECT designacao FROM cursos WHERE designacao = '{0}'""".\
                 cmd = """SELECT id_curso FROM cursos WHERE designacao = '{0}'""".\
-                    format(x[C_CURSO].replace("'", "''", 1))
+                    format(x[C_CURSO].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -272,7 +253,7 @@ class BaseDados:
                 # selecciona a referência do docente
                 #cmd = """SELECT nome_completo FROM docentes where nome_completo = '{0}'""".\
                 cmd = """SELECT id_docente FROM docentes where nome_completo = '{0}'""".\
-                    format(x[C_DOCENTE].replace("'", "''", 1))
+                    format(x[C_DOCENTE].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -344,7 +325,7 @@ class BaseDados:
                 # selecciona a referência do docente
                 #cmd = """SELECT nome_completo FROM docentes WHERE nome_completo = '{0}'""".\
                 cmd = """SELECT id_docente FROM docentes WHERE nome_completo = '{0}'""".\
-                    format(x[C_DOCENTE].replace("'", "''", 1))
+                    format(x[C_DOCENTE].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -358,7 +339,7 @@ class BaseDados:
 ##                         WHERE designacao = '{0}'""".\
                 cmd = """SELECT id_estabelecimento FROM estabelecimentos 
                          WHERE designacao = '{0}'""".\
-                    format(x[C_ESTABELECIMENTO].replace("'", "''", 1))
+                    format(x[C_ESTABELECIMENTO].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -370,7 +351,7 @@ class BaseDados:
                 # selecciona a referência da categoria
                 #cmd = """SELECT designacao FROM categorias WHERE designacao = '{0}'""".\
                 cmd = """SELECT id_categoria FROM categorias WHERE designacao = '{0}'""".\
-                    format(x[C_CATEGORIA].replace("'", "''", 1))
+                    format(x[C_CATEGORIA].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -382,7 +363,7 @@ class BaseDados:
                 # selecciona a referência do regime
                 #cmd = """SELECT designacao FROM regimes WHERE designacao = '{0}'""".\
                 cmd = """SELECT id_regime FROM regimes WHERE designacao = '{0}'""".\
-                    format(x[C_REGIME].replace("'", "''", 1))
+                    format(x[C_REGIME].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -396,7 +377,7 @@ class BaseDados:
                 #         WHERE designacao = '{0}'""".\
                 cmd = """SELECT id_tipo_estabelecimento FROM tipos_estabelecimento 
                          WHERE designacao = '{0}'""".\
-                    format(x[C_TIPO_ENSINO].replace("'", "''", 1))
+                    format(x[C_TIPO_ENSINO].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -408,7 +389,7 @@ class BaseDados:
                 # selecciona a referência do grau
                 #cmd = """SELECT designacao FROM graus WHERE designacao = '{0}'""".\
                 cmd = """SELECT id_grau FROM graus WHERE designacao = '{0}'""".\
-                    format(x[C_GRAU].replace("'", "''", 1))
+                    format(x[C_GRAU].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -419,7 +400,7 @@ class BaseDados:
                 # selecciona a referência do curso
                 #cmd = """SELECT designacao FROM cursos WHERE designacao = '{0}'""".\
                 cmd = """SELECT id_curso FROM cursos WHERE designacao = '{0}'""".\
-                    format(x[C_CURSO].replace("'", "''", 1))
+                    format(x[C_CURSO].replace("'", "''"))
                 try:
                     cursor.execute(cmd)
                     r = cursor.fetchone()
@@ -505,3 +486,4 @@ class BaseDados:
         cursor.close()
 
         pass
+    
